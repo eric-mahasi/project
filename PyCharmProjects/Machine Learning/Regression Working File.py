fb1e4570e8d19cbbc7bda55cde3eab2c026edd90
Eric Mahasi <mahasieric@gmail.com>
2019-08-14 16:27:12 +0300

    Plot data

--- PyCharmProjects/Machine Learning/Regression Working File.py
+++ PyCharmProjects/Machine Learning/Regression Working File.py
@@ -15,10 +15,12 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.1)
 
 # Save the model with the highest accuracy
 best = 0
-for _ in range(30):
+"""for _ in range(30):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
         X, y, test_size=0.1)
 
@@ -30,7 +32,7 @@ for _ in range(30):
     if accuracy > best:
         best = accuracy
         with open("student-model.pickle", "wb") as f:  # Saving the model
-            pickle.dump(linear, f)
+            pickle.dump(linear, f)"""
 
 pickle_in = open("student-model.pickle", "rb")  # Loading the model
 linear = pickle.load(pickle_in)
@@ -38,3 +40,11 @@ linear = pickle.load(pickle_in)
 predictions = linear.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+# Plotting the data
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
