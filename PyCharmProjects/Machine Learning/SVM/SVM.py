4c38e649d6ccbf5d6c52e7c9c89620b98ba70600
Eric Mahasi <mahasieric@gmail.com>
2019-08-14 23:02:15 +0300

    Train the model

--- PyCharmProjects/Machine Learning/SVM/SVM.py
+++ PyCharmProjects/Machine Learning/SVM/SVM.py
@@ -1,6 +1,7 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
 
 cancer = datasets.load_breast_cancer()
 
@@ -9,4 +10,10 @@ y = cancer.target
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     x, y, test_size=0.2)
-classes = ['malignant', 'benign']
+
+# Setting up the model
+clf = svm.SVC(kernel="linear")
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+print(accuracy)
