497c3e34733c7e806f0ebdf962b97c7e4e49c759
Eric Mahasi <mahasieric@gmail.com>
2019-08-14 16:22:16 +0300

    Save model with highest accuracy

--- PyCharmProjects/Machine Learning/Regression Working File.py
+++ PyCharmProjects/Machine Learning/Regression Working File.py
@@ -16,18 +16,23 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-    X, y, test_size=0.1)
+# Save the model with the highest accuracy
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test, y_test)
-print(accuracy)
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
 
-with open("student-model.pickle", "wb") as f:
-    pickle.dump(linear, f)
+    if accuracy > best:
+        best = accuracy
+        with open("student-model.pickle", "wb") as f:  # Saving the model
+            pickle.dump(linear, f)
 
-pickle_in = open("student-model.pickle", "rb")
+pickle_in = open("student-model.pickle", "rb")  # Loading the model
 linear = pickle.load(pickle_in)
 
 predictions = linear.predict(x_test)
