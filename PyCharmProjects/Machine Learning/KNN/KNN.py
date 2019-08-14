1f6bf5a6c8135dd9fbef2a416124f7bf1f617f93
Eric Mahasi <mahasieric@gmail.com>
2019-08-14 17:17:52 +0300

    Create model

--- PyCharmProjects/Machine Learning/KNN/KNN.py
+++ PyCharmProjects/Machine Learning/KNN/KNN.py
@@ -6,7 +6,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("car.data")
-print(data.head())
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data["buying"]))
@@ -23,3 +22,9 @@ y = list(cls)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     X, y, test_size=0.1)
+
+# Set up model
+model = KNeighborsClassifier(n_neighbors=9)
+model.fit(x_train, y_train)
+accuracy = model.score(x_test, y_test)
+print(accuracy)
