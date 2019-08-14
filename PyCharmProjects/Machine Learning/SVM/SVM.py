328c3f6c299ce36ad7ed20c0df3abac1c190483a
Eric Mahasi <mahasieric@gmail.com>
2019-08-14 22:39:52 +0300

    Initial commit

---
+++ PyCharmProjects/Machine Learning/SVM/SVM.py
@@ -0,0 +1,12 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+
+cancer = datasets.load_breast_cancer()
+
+x = cancer.data
+y = cancer.target
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    x, y, test_size=0.2)
+classes = ['malignant', 'benign']
