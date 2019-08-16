a047b5fe5309ee0e62a6409dde22ef004216eb23
Eric Mahasi <mahasieric@gmail.com>
2019-08-17 01:56:07 +0300

    Train the model

--- PyCharmProjects/Machine Learning/Logistic Regression/Logistic Regression.py
+++ PyCharmProjects/Machine Learning/Logistic Regression/Logistic Regression.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 train = pd.read_csv("titanic_train.csv")
 
@@ -30,4 +32,12 @@ train = pd.concat([train, sex, embark], axis=1)
 train.drop(['Sex', 'Embarked', 'Name', 'Ticket', 'PassengerId'], axis=1,
            inplace=True)
 print(train.head())
-plt.show()
+X = train.drop('Survived', axis=1)
+y = train['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                    random_state=101)
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+
+predictions = logmodel.predict(X_test)
+print(classification_report(y_test, predictions))
