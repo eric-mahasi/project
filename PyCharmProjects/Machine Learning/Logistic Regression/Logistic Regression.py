c708d507fba63d5517b9474b7c934e2ebc50e93f
Eric Mahasi <mahasieric@gmail.com>
2019-08-17 01:47:34 +0300

    Clean data

--- PyCharmProjects/Machine Learning/Logistic Regression/Logistic Regression.py
+++ PyCharmProjects/Machine Learning/Logistic Regression/Logistic Regression.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 train = pd.read_csv("titanic_train.csv")
+
+
 def impute_age(cols):
     Age = cols[0]
     Pclass = cols[1]
@@ -21,4 +23,11 @@ train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis=1)
 train.drop('Cabin', axis=1, inplace=True)
 train.dropna(inplace=True)
 
+sex = pd.get_dummies(train['Sex'], drop_first=True)
+embark = pd.get_dummies(train['Embarked'], drop_first=True)
+
+train = pd.concat([train, sex, embark], axis=1)
+train.drop(['Sex', 'Embarked', 'Name', 'Ticket', 'PassengerId'], axis=1,
+           inplace=True)
+print(train.head())
 plt.show()
